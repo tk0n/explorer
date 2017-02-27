f92d9bb1e98f37f898b73b50fbe887a51e4d0470
Guillaume Verbal <guillaume.verbal@gmail.com>
2017-02-27 16:15:27 +0800

    update testnet nethash

--- public/src/js/filters.js
+++ public/src/js/filters.js
@@ -53,7 +53,7 @@ angular.module('ark_explorer')
   })
   .filter('nethash', function () {
       return function (nethash) {
-          if (nethash == "da3ed6a45429278bac2666961289ca17ad86595d33b31037615d4b8e8f158bba") {
+          if (nethash == "4befbd4cd1f2f10cbe69ac0b494b5ce070595ed23ee7abd386867c4edcdaf3bd") {
               return network='Testnet';
           } else if (nethash == "ed14889723f24ecc54871d058d98ce91ff2f973192075c0155ba2b7b70ad2511")  {
 	      return network='Mainnet';
